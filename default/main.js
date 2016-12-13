163028f70a98157a107083105d2fef85ee8ca978
Cody McGinnis <brainwart@gmail.com>
2016-12-13 01:59:04 -0500

    Initial commit
    Commit original files created before using git

---
+++ default/main.js
@@ -0,0 +1,32 @@
+var tasks = require("tasks");
+
+module.exports.loop = function() {
+    for (var n in Game.spawns) {
+        var spawn = Game.spawns[n];
+        if (spawn.canCreateCreep([MOVE, MOVE, WORK, CARRY, CARRY]) == OK) {
+            var creep = spawn.createCreep([MOVE, MOVE, WORK, CARRY, CARRY]);
+        }
+    }
+    
+    
+    
+    for (var n in Game.creeps) {
+        var creep = Game.creeps[n];
+        if (creep.spawning)
+            continue;
+            
+        var taskdest = Game.getObjectById(creep.memory.taskdest);
+        
+        if (creep.memory.task && Game.getObjectById(creep.memory.taskdest)) {
+            var result = tasks[creep.memory.task](creep, taskdest);
+            
+            if (result == ERR_NOT_IN_RANGE) {
+                creep.moveTo(taskdest, {reusePath : 10, serializeMemory : true});
+            } else if (result == tasks.COMPLETED) {
+                tasks.new(creep);
+            }
+        } else {
+            tasks.new(creep);
+        }
+    }
+}
---
+++ default/tasks.js
@@ -0,0 +1,90 @@
+var tasks = {}
+tasks.setup = {}
+tasks.COMPLETED = 10001
+
+tasks.harvest = function(creep, toharvest) {
+    if (creep.carry.energy < creep.carryCapacity && toharvest instanceof Source) {
+        return creep.harvest(toharvest);
+    }
+    
+    return tasks.COMPLETED;
+}
+tasks.setup.harvest = function (creep) {
+    creep.memory.task = "harvest";
+    var toharvest = creep.pos.findClosestByPath(FIND_SOURCES);
+    if (toharvest)
+        creep.memory.taskdest = toharvest.id;
+    else
+        creep.memory.taskdest = null;
+}
+
+tasks.build = function(creep, tobuild) {
+    if (creep.carry.energy < 1)
+        return tasks.COMPLETED;
+
+    if (tobuild) 
+        return creep.build(tobuild);
+}
+tasks.setup.build = function(creep) {
+	creep.memory.task = "build";
+	var tobuild = creep.pos.findClosestByPath(FIND_MY_CONSTRUCTION_SITES);
+	if (tobuild)
+		creep.memory.taskdest = tobuild.id;
+	else
+		creep.memory.taskdest = null;
+}
+
+tasks.upgrade = function(creep, controller) {
+    if (creep.carry.energy < 1)
+        return tasks.COMPLETED;
+
+    if (controller) 
+        return creep.upgradeController(controller);
+}
+tasks.setup.upgrade = function(creep) {
+    creep.memory.task = "upgrade";
+    if (creep.room.controller.my)
+        creep.memory.taskdest = creep.room.controller.id;
+    else
+        creep.memory.taskdest = null;
+}
+tasks.transfer = function(creep, to) {
+    if (creep.carry.energy < 1)
+        return tasks.COMPLETED;
+    return creep.transfer(to, RESOURCE_ENERGY);
+}
+tasks.setup.transfer = function(creep) {
+    creep.memory.task = "transfer";
+    
+    var transferstruct = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+        filter : (structure) => { return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
+                            structure.energy < structure.energyCapacity; }
+    });
+    
+    if (transferstruct)
+        creep.memory.taskdest = transferstruct.id;
+    else
+        creep.memory.taskdest = null;
+}
+tasks.new = function(creep) {
+    if (creep.getActiveBodyparts(WORK) > 0) {
+        if (creep.carry.energy < 1) {
+            tasks.setup.harvest(creep);
+            return;
+        }
+        
+        var nextOrder = ([
+    		tasks.setup.upgrade,
+    		tasks.setup.upgrade,
+    		tasks.setup.build,
+    		tasks.setup.transfer,
+    		tasks.setup.transfer
+        ])[Math.floor(Math.random() * 5)];
+    	if (nextOrder && creep.memory.taskdest) {
+    		nextOrder(creep);
+    		console.log(creep.memory.task);
+    	} else
+    		creep.say("NO ORDERS");
+    }
+}
+module.exports = tasks;
