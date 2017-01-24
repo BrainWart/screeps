557db4573d39b28306b9bdab42f113e3920d25eb
Cody McGinnis <brainwart@gmail.com>
2017-01-23 20:22:28 -0500

    fix memory deletion

--- default/prototype.Creep.js
+++ default/prototype.Creep.js
@@ -2,6 +2,7 @@ let roles = require("data").roles;
 
 module.exports = function() {
 	Creep.prototype.work = function() {
+		// Kill old creeps and remove old memory
 		if (this.ticksToLive < 100) {
 			Memory.creeps[this.name] = null;
 			this.say("I'm dying!!!!.... ehhh", false);
@@ -9,8 +10,9 @@ module.exports = function() {
 			return;
 		}
 
+		// Kill creeps with broken memory
 		if (_.isNull(this.memory) || _.isNull(this.memory.role)) {
-			Memory.creeps[this.name] = null;
+			delete Memory.creeps[this.name];
 			this.say("I'm broken!!  *dead*", false);
 			this.suicide();
 			return;
