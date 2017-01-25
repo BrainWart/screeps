6b41ba0bfa4dc4ed9359bc423bf1182064a3a4dc
Cody McGinnis <brainwart@gmail.com>
2017-01-24 19:53:30 -0500

    Redo creep main logic and changes to creep prototype

--- default/main.js
+++ default/main.js
@@ -3,12 +3,28 @@ require("prototype.Creep")();
 var roles = require("data").roles;
 
 module.exports.loop = function() {
-	{ // SPAWN NEW CREEPS
-	}
-    
+    let roleCounts = {}
+	_.forEach(roles, (obj, name) => (roleCounts[name] = 0));
+
     { // CREEP LOGIC
-		for (var cn in Game.creeps) {
-			Game.creeps[cn].work();
+		for (var cn in Memory.creeps) {
+			if (Game.creeps[cn]) {
+				let creep = Game.creeps[cn];
+				let role = Memory.creeps[cn].role;
+				Game.creeps[cn].work();
+				if (role)
+					roleCounts[role]++;
+			} else {
+				delete Memory.creeps[cn];
+			}
 		}
 	}
+
+	/* { // SPAWN NEW CREEPS
+		for (var role in roles) {
+			if (roles[role].shouldSpawn(roleCount)) {
+				
+			}
+		}
+	} //*/
 }
--- default/prototype.Creep.js
+++ default/prototype.Creep.js
@@ -2,22 +2,23 @@ let roles = require("data").roles;
 
 module.exports = function() {
 	Creep.prototype.work = function() {
+		// Don't waste cycles on a creep doing nothing
+		if (this.spawning) {
+			return;
+		}
+
 		// Kill old creeps and remove old memory
 		if (this.ticksToLive < 100) {
-			Memory.creeps[this.name] = null;
-			this.say("I'm dying!!!!.... ehhh", false);
 			this.suicide();
 			return;
 		}
 
 		// Kill creeps with broken memory
 		if (_.isNull(this.memory) || _.isEmpty(this.memory) || _.isNull(this.memory.role)) {
-			delete Memory.creeps[this.name];
-			this.say("I'm broken!!  *dead*", false);
 			this.suicide();
 			return;
 		}
 
 		roles[this.memory.role].run(this);
-	}
+	};
 };
